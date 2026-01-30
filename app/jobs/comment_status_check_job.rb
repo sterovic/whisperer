45e663e90acb90e5a54feab1e6c0db2584c9663c
Senadin Terovic <senadinterovic96@gmail.com>
2026-01-30 22:18:07 +0100

    Fix comment check | Polish comments table

--- app/jobs/comment_status_check_job.rb
+++ app/jobs/comment_status_check_job.rb
@@ -38,51 +38,27 @@ class CommentStatusCheckJob < ApplicationJob
     video = comment.video
     yt_video = Yt::Video.new(id: video.youtube_id)
 
-    # Fetch top-level comments from the video
-    video_comments = fetch_video_comments(yt_video)
+    # Try calling some method on a Yt::CommentThread to trigger the API call
+    # If no items are returned, the comment doesn't exist - it's been removed
+    yt_comment = Yt::CommentThread.new(id: comment.youtube_comment_id)
+    yt_comment.text_display
 
-    # Check if our comment is in the fetched comments
-    found_comment = video_comments.find { |yt_comment| yt_comment.id == comment.youtube_comment_id }
-
-    if found_comment
-      # Comment is visible in the video's comment list
-      update_comment_from_yt(comment, found_comment, video_comments)
-      comment.visible! unless comment.visible?
-    elsif comment_exists_directly?(comment.youtube_comment_id)
-      # Comment exists but not in the top comments list (hidden from top)
-      comment.hidden! unless comment.hidden?
-      Rails.logger.info "Comment #{comment.id} is hidden from top"
-    else
-      # Comment doesn't exist at all (removed)
-      comment.removed! unless comment.removed?
-      Rails.logger.info "Comment #{comment.id} has been removed"
+    # Now let's check if the comment is visible in the video's comment list
+    matched = false
+    rank = 0
+    yt_video.comment_threads.where(order: :relevance).each do |thread|
+      rank += 1
+      if thread.id == comment.youtube_comment_id
+        matched = true
+        break
+      end
     end
-  end
-
-  def fetch_video_comments(yt_video)
-    yt_video.comment_threads.where(order: :relevance).take(9999)
-  rescue Yt::Errors::Forbidden, Yt::Errors::RequestError
-    Rails.logger.warn "Cannot fetch comments for video: #{yt_video.id}"
-    []
-  end
-
-  def comment_exists_directly?(youtube_comment_id)
-    # Try to fetch the comment thread directly by ID
-    comment_thread = Yt::CommentThread.new(id: youtube_comment_id)
-    # Try to access a property to trigger the API call
-    comment_thread.text_display.present?
-  rescue Yt::Errors::NoItems, Yt::Errors::Forbidden, Yt::Errors::RequestError
-    false
-  end
-
-  def update_comment_from_yt(comment, yt_comment, all_comments)
-    # Find rank (position in the list)
-    rank = all_comments.index { |c| c.id == comment.youtube_comment_id }
-
-    # Update like count and rank
-    comment.update(
-      like_count: yt_comment.like_count || 0,
-      rank: rank ? rank + 1 : nil
-    )
+    if matched
+      comment.update!(like_count: yt_comment.like_count || 0, rank: rank, status: :visible)
+    else
+      comment.update!(status: :hidden, rank: nil)
+    end
+  rescue Yt::Errors::NoItems => e
+    comment.update!(status: :removed, rank: nil)
   end
 end
